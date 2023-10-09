16de7ce279eb20c79790db3f7edccb37d10485ca
dangkhuong14 <f13file31@gmail.com>
2023-10-09 15:24:23 +0700

    Create some basic validations for MovieSerializer

--- watchlist_app/api/serializers.py
+++ watchlist_app/api/serializers.py
@@ -2,10 +2,15 @@ from rest_framework import serializers
 from watchlist_app.models import Movie
 
 
+def description_length(value):
+    if len(value) < 5:
+        raise serializers.ValidationError('Description is too short!')
+
+
 class MovieSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
     name = serializers.CharField()
-    description = serializers.CharField()
+    description = serializers.CharField(validators=[description_length])
     active = serializers.BooleanField()
 
     def create(self, validated_data):
@@ -19,3 +24,16 @@ class MovieSerializer(serializers.Serializer):
         # Neu model da ton tai trong db thi ham save() se tao UPDATE statement nguoc lai se tao INSERT statement
         instance.save()
         return instance
+
+    def validate_name(self, value):
+        if len(value) < 2:
+            raise serializers.ValidationError('Name is too short!')
+        else:
+            return value
+
+    def validate(self, data):
+        if data['name'] == data['description']:
+            raise serializers.ValidationError(
+                'Name and description should not be the same!')
+        else:
+            return data
