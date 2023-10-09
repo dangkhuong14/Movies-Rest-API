f386985324822ad6e41cdc188470deffbf4f7fe9
dangkhuong14 <f13file31@gmail.com>
2023-10-09 10:04:50 +0700

    Switch from function based view to class based view

--- watchlist_app/api/views.py
+++ watchlist_app/api/views.py
@@ -1,3 +1,4 @@
+from rest_framework.views import APIView
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
@@ -6,16 +7,14 @@ from watchlist_app.models import Movie
 from django.http import JsonResponse
 from watchlist_app.api.serializers import MovieSerializer
 
-# Create your views here.
 
-
-@api_view(['GET', 'POST'])
-def movie_list(request):
-    if (request.method == 'GET'):
+class MovieListAV(APIView):
+    def get(self, request):
         movies = Movie.objects.all()
         serializer = MovieSerializer(movies, many=True)
         return Response(serializer.data)
-    if (request.method == 'POST'):
+
+    def post(self, request):
         serializer = MovieSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
@@ -24,9 +23,8 @@ def movie_list(request):
             return Response(serializer.errors)
 
 
-@api_view(['GET', 'PUT', 'DELETE'])
-def movie_details(request, pk):
-    if request.method == 'GET':
+class MovieDetailAV(APIView):
+    def get(self, request, pk):
         try:
             movie = Movie.objects.get(pk=pk)
         except Movie.DoesNotExist:
@@ -34,18 +32,59 @@ def movie_details(request, pk):
                             status=status.HTTP_404_NOT_FOUND)
         serializer = MovieSerializer(movie)
         return Response(serializer.data)
-    if request.method == 'PUT':
+
+    def put(self, request, pk):
         movie = Movie.objects.get(pk=pk)
-        # Serializer object biet duoc khi nao goi ham create() hay update() trong serializer class
-        # bang cach kiem tra thuoc tinh instance chua Model instance co dang duoc serializer xu ly
-        # cu the o day la movie variable
         serializer = MovieSerializer(movie, request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
         else:
             return Response(serializer.errors)
-    if request.method == 'DELETE':
+
+    def delete(self, request, pk):
         movie = Movie.objects.get(pk=pk)
         movie.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+# @api_view(['GET', 'POST'])
+# def movie_list(request):
+#     if (request.method == 'GET'):
+#         movies = Movie.objects.all()
+#         serializer = MovieSerializer(movies, many=True)
+#         return Response(serializer.data)
+#     if (request.method == 'POST'):
+#         serializer = MovieSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+#         else:
+#             return Response(serializer.errors)
+
+
+# @api_view(['GET', 'PUT', 'DELETE'])
+# def movie_details(request, pk):
+#     if request.method == 'GET':
+#         try:
+#             movie = Movie.objects.get(pk=pk)
+#         except Movie.DoesNotExist:
+#             return Response({'error': 'Movie not found'},
+#                             status=status.HTTP_404_NOT_FOUND)
+#         serializer = MovieSerializer(movie)
+#         return Response(serializer.data)
+#     if request.method == 'PUT':
+#         movie = Movie.objects.get(pk=pk)
+#         # Serializer object biet duoc khi nao goi ham create() hay update() trong serializer class
+#         # bang cach kiem tra thuoc tinh instance chua Model instance co dang duoc serializer xu ly
+#         # cu the o day la movie variable
+#         serializer = MovieSerializer(movie, request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+#         else:
+#             return Response(serializer.errors)
+#     if request.method == 'DELETE':
+#         movie = Movie.objects.get(pk=pk)
+#         movie.delete()
+#         return Response(status=status.HTTP_204_NO_CONTENT)
