3abd75c85c016d8d416b05dd943c330ac7713b20
dangkhuong14 <f13file31@gmail.com>
2023-10-09 08:57:26 +0700

    Create 404 not found response with specific movie

--- watchlist_app/api/views.py
+++ watchlist_app/api/views.py
@@ -27,7 +27,11 @@ def movie_list(request):
 @api_view(['GET', 'PUT', 'DELETE'])
 def movie_details(request, pk):
     if request.method == 'GET':
-        movie = Movie.objects.get(pk=pk)
+        try:
+            movie = Movie.objects.get(pk=pk)
+        except Movie.DoesNotExist:
+            return Response({'error': 'Movie not found'},
+                            status=status.HTTP_404_NOT_FOUND)
         serializer = MovieSerializer(movie)
         return Response(serializer.data)
     if request.method == 'PUT':
