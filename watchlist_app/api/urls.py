c4e1458584426f359c87ceb750f78e1afbc6b1c8
dangkhuong14 <f13file31@gmail.com>
2023-10-10 10:11:05 +0700

    Rename urls

--- watchlist_app/api/urls.py
+++ watchlist_app/api/urls.py
@@ -2,9 +2,10 @@ from django.urls import path
 from watchlist_app.api.views import WatchListAV, WatchDetailAV, StreamPlatformAV, StreamPlatformDetailAV
 
 urlpatterns = [
-    path('list/', WatchListAV.as_view(), name='movie-list'),
-    path('<int:pk>/', WatchDetailAV.as_view(), name='movie-detail'),
-    path('stream/', StreamPlatformAV.as_view(), name='stream'),
+    path('list/', WatchListAV.as_view(), name='watch-list'),
+    path('<int:pk>/', WatchDetailAV.as_view(), name='watch-detail'),
+    path('stream/', StreamPlatformAV.as_view(),
+         name='stream-list'),
     path('stream/<int:pk>/', StreamPlatformDetailAV.as_view(),
-         name='stream-platform-detail'),
+         name='stream-detail'),
 ]
