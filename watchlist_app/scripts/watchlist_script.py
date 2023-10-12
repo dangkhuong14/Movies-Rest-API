9a524cd6384ae70525d6a5ea86f2b74ecf8282e0
dangkhuong14 <f13file31@gmail.com>
2023-10-12 15:43:47 +0700

    Ignore __pycache__ and /scripts/

--- watchlist_app/scripts/watchlist_script.py
+++
@@ -1,9 +0,0 @@
-from django.db import connection
-from watchlist_app.models import Review
-from pprint import pprint
-
-
-def run():
-    review_queryset = Review.objects.all()
-    pprint(review_queryset)
-    pprint(connection.queries)
